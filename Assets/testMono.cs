82056735e7971cf29da3ce8af0df786056eb3f0b
GregoryMaltezos <grigorismalt@gmail.com>
2026-02-11 14:26:48 +0200

    TESTING 1

--- Assets/testMono.cs
+++ Assets/testMono.cs
@@ -1,8 +1,21 @@
+using kar_main_utils;
+using karianakisEditTools;
 using UnityEngine;
 
 public class testMono : MonoBehaviour
 {
-
+  void Start()
+    {
+        invo.simple(action:()=>{Debug.Log("WORKS");}, delay: 2f);
+        EditTerminal.AddCommand("greg", testing);
+        EditTerminal.AddCommand("malt", ()=>{Debug.Log("WOooooooooooRKS");});
+        ShortcutAction.Create("testing", ()=>{Debug.Log("WORKS");},KeyCode.K);
+    }
  // just a test mono to comit 
+
+  void testing()
+    {
+      Debug.Log("wooooooooooooooooooooooooooork");
+    }
 
 }
